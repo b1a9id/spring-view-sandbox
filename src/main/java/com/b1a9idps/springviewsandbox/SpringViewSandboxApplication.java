4ffcf4946749e68d5904501ce276fae86bc779cd
Ryosuke Uchitate <ruchitate@st.inc>
2023-11-17 09:56:00 +0900

    Initial Commit

---
+++ src/main/java/com/b1a9idps/springviewsandbox/SpringViewSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.b1a9idps.springviewsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringViewSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringViewSandboxApplication.class, args);
+	}
+
+}
