63f1f20124fbdc47c99dc977dbd6240d0a12a8bf
Ryosuke Uchitate <ruchitate@st.inc>
2023-11-17 10:23:00 +0900

    不要なアノテーション削除

--- src/main/java/com/b1a9idps/springviewsandbox/config/WebMvcConfig.java
+++ src/main/java/com/b1a9idps/springviewsandbox/config/WebMvcConfig.java
@@ -1,12 +1,10 @@
 package com.b1a9idps.springviewsandbox.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration(proxyBeanMethods = false)
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
